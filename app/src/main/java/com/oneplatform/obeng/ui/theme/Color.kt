4d8ea5a67eb88170c2ca9525846bf68d7d6042a4
Rakha Satria Pratama <101621066+RakhaSatriaP@users.noreply.github.com>
2023-06-02 22:41:20 +0700

    Menambahkan asset warna2

--- app/src/main/java/com/oneplatform/obeng/ui/theme/Color.kt
+++ app/src/main/java/com/oneplatform/obeng/ui/theme/Color.kt
@@ -3,15 +3,15 @@ package com.oneplatform.obeng.ui.theme
 import androidx.compose.ui.graphics.Color
 
 
-
 val primary = Color(0xFFFF5757)
 val Red20 = Color(0xFFFF5757)
 val Red100 = Color(0xFFF86B6B)
-val White10 = Color(0xFFD9D9D9)
-
+val White10 = Color(0xFFF8F8F8)
+val secondary =  Color(0xFFF86B6B)
 val gray = Color(0xFF4B4F5A)
 val dark_gray = Color(0xFF54555A)
 val light_gray = Color(0xFF696969)
+val orange = Color(0xFFFF5722)
 val text_hint_color = Color(0xFFA7A7A7)
 
 val splash_color = Color(0xFFFF5757)
