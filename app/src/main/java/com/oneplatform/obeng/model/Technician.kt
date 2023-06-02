46936b06a179b61e703879f0a5515a07329b0368
Nabhan Yuzqi <30825747+nabhanyuzqi1@users.noreply.github.com>
2023-06-02 23:53:18 +0700

    Delete app directory

--- app/src/main/java/com/oneplatform/obeng/model/Technician.kt
+++
@@ -1,5 +0,0 @@
-package com.oneplatform.obeng.model
-
-data class Technician(val id: String, val nama: String, val email: String, val noHandphone: String,
-                      val keahlian: String, val linkSertifikasi: String, val linkPortofolio: String,
-                      val jenisKeahlian: String, val role: String)
