e3b43fb51e8a9e1a9985d8a719ae8f245a0b8598
AuliaAngg <128691238+AuliaAngg@users.noreply.github.com>
2023-06-13 16:33:01 +0700

    Update technicians.js

--- src/technicians.js
+++ src/technicians.js
@@ -38,6 +38,30 @@ techniciansRouter.post(
       } = req.body;
       const { fotoKTP, fotoProfil } = req.files;
 
+      // Check if username, email, noHandphone, or NIK already exists
+      const existingTechnicianEmail = await techniciansCollection
+        .where("email", "==", email)
+        .get();
+
+      const existingTechnicianNoHandphone = await techniciansCollection
+        .where("noHandphone", "==", noHandphone)
+        .get();
+
+      const existingTechnicianNIK = await techniciansCollection
+        .where("NIK", "==", NIK)
+        .get();
+
+      if (
+        !existingTechnicianEmail.empty ||
+        !existingTechnicianNoHandphone.empty ||
+        !existingTechnicianNIK.empty
+      ) {
+        return res.status(400).json({
+          status: "error",
+          error: "Username, email, noHandphone, or NIK already exists",
+        });
+      }
+
       const userRecord = await admin.auth().createUser({
         email,
         password,
@@ -148,191 +172,205 @@ techniciansRouter.get("/data", (req, res) => {
 
 // Endpoint untuk mengubah data teknisi (technicians)
 techniciansRouter.put(
-    "/data/:technicianId",
-    upload.fields([
-      { name: "fotoKTP", maxCount: 1 },
-      { name: "fotoProfil", maxCount: 1 },
-    ]),
-    async (req, res) => {
-      try {
-        const technicianId = req.params.technicianId;
-        const {
-          nama,
-          email,
-          password,
-          noHandphone,
-          keahlian,
-          NIK,
-          linkSertifikasi,
-          linkPortofolio,
-          jenisKendaraan,
-        } = req.body;
-        const { fotoKTP, fotoProfil } = req.files;
-  
-        const technician = {
-          nama,
-          email,
-          password,
-          noHandphone,
-          keahlian,
-          NIK,
-          linkSertifikasi,
-          linkPortofolio,
-          jenisKendaraan,
-        };
-  
-        const bucket = storage.bucket("gs://loginsignup-auth-dc6a9.appspot.com");
-  
-        // URL foto KTP dan foto profil yang diunggah
-        let fotoKTPUrl = "";
-        let fotoProfilUrl = "";
-  
-        if (fotoKTP) {
-          let uuid = UUID();
-          const downloadPath =
-            "https://firebasestorage.googleapis.com/v0/b/loginsignup-auth-dc6a9.appspot.com/o/";
-  
-          const fotoKTPResponse = await bucket.upload(fotoKTP[0].path, {
-            destination: `techniciansKTP/${fotoKTP[0].originalname}`,
-            resumable: true,
-            metadata: {
-              metadata: {
-                firebaseStorageDownloadTokens: uuid,
-              },
-            },
-          });
-  
-          // URL foto KTP
-          fotoKTPUrl =
-            downloadPath +
-            encodeURIComponent(fotoKTPResponse[0].name) +
-            "?alt=media&token=" +
-            uuid;
-        }
-  
-        if (fotoProfil) {
-          let uuid = UUID();
-          const downloadPath =
-            "https://firebasestorage.googleapis.com/v0/b/loginsignup-auth-dc6a9.appspot.com/o/";
-  
-          const fotoProfilResponse = await bucket.upload(fotoProfil[0].path, {
-            destination: `techniciansProfil/${fotoProfil[0].originalname}`,
-            resumable: true,
-            metadata: {
-              metadata: {
-                firebaseStorageDownloadTokens: uuid,
-              },
-            },
-          });
-  
-          // URL foto profil
-          fotoProfilUrl =
-            downloadPath +
-            encodeURIComponent(fotoProfilResponse[0].name) +
-            "?alt=media&token=" +
-            uuid;
-        }
-  
-        if (fotoKTPUrl !== "") {
-          technician.fotoKTP = fotoKTPUrl;
-        }
-        if (fotoProfilUrl !== "") {
-          technician.fotoProfil = fotoProfilUrl;
-        }
-  
-        await techniciansCollection.doc(technicianId).update(technician);
-  
-        res.status(200).json({
-          status: "success",
-          technicianId: technicianId,
-          message: "Data teknisi berhasil diubah!",
-        });
-      } catch (error) {
-        res.status(500).json({
+  "/data/:technicianId",
+  upload.fields([
+    { name: "fotoKTP", maxCount: 1 },
+    { name: "fotoProfil", maxCount: 1 },
+  ]),
+  async (req, res) => {
+    try {
+      const technicianId = req.params.technicianId;
+      const {
+        nama,
+        email,
+        password,
+        noHandphone,
+        keahlian,
+        NIK,
+        linkSertifikasi,
+        linkPortofolio,
+        jenisKendaraan,
+      } = req.body;
+      const { fotoKTP, fotoProfil } = req.files;
+
+      // Check if username, email, noHandphone, or NIK already exists
+      const existingTechnicianEmail = await techniciansCollection
+        .where("email", "==", email)
+        .get();
+
+      const existingTechnicianNoHandphone = await techniciansCollection
+        .where("noHandphone", "==", noHandphone)
+        .get();
+
+      const existingTechnicianNIK = await techniciansCollection
+        .where("NIK", "==", NIK)
+        .get();
+
+      if (
+        !existingTechnicianEmail.empty ||
+        !existingTechnicianNoHandphone.empty ||
+        !existingTechnicianNIK.empty
+      ) {
+        return res.status(400).json({
           status: "error",
-          error: "Gagal mengubah data teknisi.",
+          error: "Username, email, noHandphone, or NIK already exists",
         });
       }
+
+      const technician = {
+        nama,
+        email,
+        password,
+        noHandphone,
+        keahlian,
+        NIK,
+        linkSertifikasi,
+        linkPortofolio,
+        jenisKendaraan,
+      };
+
+      const bucket = storage.bucket("gs://loginsignup-auth-dc6a9.appspot.com");
+
+      // URL foto KTP dan foto profil yang diunggah
+      let fotoKTPUrl = "";
+      let fotoProfilUrl = "";
+
+      if (fotoKTP && fotoKTP.length > 0) {
+        const fotoKTPResponse = await bucket.upload(fotoKTP[0].path, {
+          destination: `techniciansKTP/${fotoKTP[0].originalname}`,
+          resumable: true,
+          metadata: {
+            metadata: {
+              firebaseStorageDownloadTokens: uuid(),
+            },
+          },
+        });
+
+        // URL foto KTP
+        fotoKTPUrl = fotoKTPResponse[0].publicUrl();
+      }
+
+      if (fotoProfil && fotoProfil.length > 0) {
+        const fotoProfilResponse = await bucket.upload(fotoProfil[0].path, {
+          destination: `techniciansProfil/${fotoProfil[0].originalname}`,
+          resumable: true,
+          metadata: {
+            metadata: {
+              firebaseStorageDownloadTokens: uuid(),
+            },
+          },
+        });
+
+        // URL foto profil
+        fotoProfilUrl = fotoProfilResponse[0].publicUrl();
+      }
+
+      if (fotoKTPUrl !== "") {
+        technician.fotoKTP = fotoKTPUrl;
+      }
+      if (fotoProfilUrl !== "") {
+        technician.fotoProfil = fotoProfilUrl;
+      }
+
+      await techniciansCollection.doc(technicianId).update(technician);
+
+      res.status(200).json({
+        status: "success",
+        technicianId: technicianId,
+        message: "Data teknisi berhasil diubah!",
+      });
+    } catch (error) {
+      console.error("Error updating technician:", error);
+      res.status(500).json({
+        status: "error",
+        error: "Gagal mengubah data teknisi.",
+      });
     }
+  }
 );
 
 // Endpoint untuk mengunggah data feedback dan rating untuk teknisi (technicians)
 techniciansRouter.post("/:technicianId/feedback", async (req, res) => {
-    try {
-      const { technicianId } = req.params;
-      const { pesan, rating } = req.body;
-  
-      // Validasi rating agar berada dalam rentang 1 hingga 5
-      if (rating < 1 || rating > 5) {
-        return res
-          .status(400)
-          .json({ error: "Rating harus berada dalam rentang 1 hingga 5" });
-      }
-  
-      // Dapatkan referensi dokumen teknisi
-      const technicianRef = techniciansCollection.doc(technicianId);
-  
-      // Periksa apakah teknisi tersebut ada
-      const technicianDoc = await technicianRef.get();
-      if (!technicianDoc.exists) {
-        return res.status(404).json({ error: "Teknisi tidak ditemukan" });
-      }
-  
-      // Buat objek feedback baru dengan ID yang dihasilkan otomatis oleh Firestore
-      const feedback = {
-        pesan,
-        rating,
-        timestamp: admin.firestore.FieldValue.serverTimestamp(),
-      };
-  
-      // Tambahkan feedback ke sub-koleksi "feedbacks" pada dokumen teknisi
-      const feedbackRef = await technicianRef.collection("feedbacks").add(feedback);
-  
-      // Perbarui feedbackId pada dokumen teknisi
-      await technicianRef.update({ feedbackId: feedbackRef.id });
-  
-      res.status(200).json({
-        status: "success",
-        feedbackId: feedbackRef.id,
-        message: "Feedback dan rating berhasil diperbarui",
-      });
-    } catch (error) {
-      console.error("Terjadi kesalahan saat memperbarui feedback dan rating:", error);
-      res.status(500).json({
-        status: "error",
-        message: "Gagal memperbarui feedback dan rating",
-      });
+  try {
+    const { technicianId } = req.params;
+    const { pesan, rating } = req.body;
+
+    // Validasi rating agar berada dalam rentang 1 hingga 5
+    if (rating < 1 || rating > 5) {
+      return res
+        .status(400)
+        .json({ error: "Rating harus berada dalam rentang 1 hingga 5" });
     }
-  });
-  
-  // Endpoint untuk mengambil feedback data teknisi berdasarkan id teknisi (technicians)
-  techniciansRouter.get("/:technicianId/feedbacks", async (req, res) => {
-    try {
-      const { technicianId } = req.params;
-  
-      // Dapatkan referensi koleksi feedbacks dari teknisi yang sesuai
-      const feedbacksSnapshot = await techniciansCollection
-        .doc(technicianId)
-        .collection("feedbacks")
-        .get();
-  
-      // Buat array untuk menyimpan feedback
-      const feedbacks = [];
-  
-      // Loop melalui setiap dokumen feedback dan tambahkan ke array feedbacks
-      feedbacksSnapshot.forEach((doc) => {
-        const feedback = doc.data();
-        feedbacks.push(feedback);
-      });
-  
-      res.json({
-        status: "sukses",
-        feedbacks,
-      });
-    } catch (error) {
-      console.error("Terjadi kesalahan saat mendapatkan feedback:", error);
-      res.status(500).json({ error: "Gagal mendapatkan feedback" });
+
+    // Dapatkan referensi dokumen teknisi
+    const technicianRef = techniciansCollection.doc(technicianId);
+
+    // Periksa apakah teknisi tersebut ada
+    const technicianDoc = await technicianRef.get();
+    if (!technicianDoc.exists) {
+      return res.status(404).json({ error: "Teknisi tidak ditemukan" });
     }
+
+    // Buat objek feedback baru dengan ID yang dihasilkan otomatis oleh Firestore
+    const feedback = {
+      pesan,
+      rating,
+      timestamp: admin.firestore.FieldValue.serverTimestamp(),
+    };
+
+    // Tambahkan feedback ke sub-koleksi "feedbacks" pada dokumen teknisi
+    const feedbackRef = await technicianRef
+      .collection("feedbacks")
+      .add(feedback);
+
+    // Perbarui feedbackId pada dokumen teknisi
+    await technicianRef.update({ feedbackId: feedbackRef.id });
+
+    res.status(200).json({
+      status: "success",
+      feedbackId: feedbackRef.id,
+      message: "Feedback dan rating berhasil diperbarui",
+    });
+  } catch (error) {
+    console.error(
+      "Terjadi kesalahan saat memperbarui feedback dan rating:",
+      error
+    );
+    res.status(500).json({
+      status: "error",
+      message: "Gagal memperbarui feedback dan rating",
+    });
+  }
+});
+
+// Endpoint untuk mengambil feedback data teknisi berdasarkan id teknisi (technicians)
+techniciansRouter.get("/:technicianId/feedbacks", async (req, res) => {
+  try {
+    const { technicianId } = req.params;
+
+    // Dapatkan referensi koleksi feedbacks dari teknisi yang sesuai
+    const feedbacksSnapshot = await techniciansCollection
+      .doc(technicianId)
+      .collection("feedbacks")
+      .get();
+
+    // Buat array untuk menyimpan feedback
+    const feedbacks = [];
+
+    // Loop melalui setiap dokumen feedback dan tambahkan ke array feedbacks
+    feedbacksSnapshot.forEach((doc) => {
+      const feedback = doc.data();
+      feedbacks.push(feedback);
+    });
+
+    res.json({
+      status: "sukses",
+      feedbacks,
+    });
+  } catch (error) {
+    console.error("Terjadi kesalahan saat mendapatkan feedback:", error);
+    res.status(500).json({ error: "Gagal mendapatkan feedback" });
+  }
 });
 
 module.exports = techniciansRouter;
